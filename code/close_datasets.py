569b78418481f4fb8cd02bc3a6392c65876b0a8f
Kyle M. Kabasares <46383246+kylekaba@users.noreply.github.com>
2024-04-21 14:32:33 -0700

    Delete code directory

--- code/close_datasets.py
+++
@@ -1,7 +0,0 @@
-def close_datasets(*args):
-    """
-    Close any open rioxarray datasets to free up memory.
-    """
-    for arg in args:
-        if isinstance(arg, rio.rioxarray.RasterArray):
-            arg.close()
